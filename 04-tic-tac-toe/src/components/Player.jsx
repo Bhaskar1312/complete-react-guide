b3dcab4d2832e727daba23ef3e82fbd011844567
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-26 02:19:31 +0530

    04 - two way binding, saving input successful

--- 04-tic-tac-toe/src/components/Player.jsx
+++ 04-tic-tac-toe/src/components/Player.jsx
@@ -1,7 +1,16 @@
 import {useState} from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
+
+    function handleChange(event) {
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+        setPlayerName(event.target.value);
+    }
+
     function handleEditClick(name) {
         console.log('clicked on button', name);
         // setIsEditing(!isEditing);
@@ -9,18 +18,19 @@ export default function Player({name, symbol}) {
         setIsEditing(wasEditing => !wasEditing); // best practice when current depends on prev state instead of !isEditing
         // setIsEditing(wasEditing => !wasEditing); // nothing happens when both are functions
     }
-    // let playerName = (<span className="player-name">{name}</span>)
+    // let playerName = (<span className="player-name" defaultValue={name}>{name}</span>)
     // if(isEditing) {
     //     playerName = (<input type="text" required></input>);
     // }
     return (
         <li>
             <span className="player">
-              {!isEditing && (<span className="player-name">{name}</span>)}
+              {!isEditing && (<span className="player-name">{playerName}</span>)}
               {/*  {playerName}*/}
               <span className="player-symbol">{symbol}</span>
-                {isEditing && (<input type="text" value={name} required></input>)}
-              <button onClick={()=>handleEditClick(name)}>{isEditing ? "Save" : "Edit"}</button>
+                {/*defaultValue={name} so that you can change*/}
+                {isEditing && (<input type="text" value={playerName} onChange={handleChange} required></input>)}
+              <button onClick={()=>handleEditClick(initialName)}>{isEditing ? "Save" : "Edit"}</button>
             </span>
         </li>
     );
