d6df36ac1b3a4388652475f1708d393199002fb5
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-25 20:27:14 +0530

    03: outputting list data dynamically

--- 03-react-fundamentals/src/App.jsx
+++ 03-react-fundamentals/src/App.jsx
@@ -42,15 +42,21 @@ function App() {
                 <section id="core-concepts" >
                     <h2>Time to get started!</h2>
                     <ul>
-                        <CoreConcept
-                            title={CORE_CONCEPTS[0].title}
-                            description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image}
-                        />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        {/*    they will be added as key/val pairs using spread operator */}
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                        {CORE_CONCEPTS.map((conceptItem)=>(
+                            <CoreConcept key={conceptItem.title} {...conceptItem} />
+                        ))}
+                        {/* Warning: Each child in a list should have a unique "key" prop.*/}
+
+                        {/*<CoreConcept*/}
+                        {/*    title={CORE_CONCEPTS[0].title}*/}
+                        {/*    description={CORE_CONCEPTS[0].description}*/}
+                        {/*    image={CORE_CONCEPTS[0].image}*/}
+                        {/*/>*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[1]} />*/}
+                        {/*/!*    they will be added as key/val pairs using spread operator *!/*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[2]} />*/}
+                        {/*<CoreConcept {...CORE_CONCEPTS[3]} />*/}
+
                         {/*<CoreConcept concept={CORE_CONCEPTS[0]} />*/}
                         {/*export default function CoreConcept({ concept }) {}*/}
                         {/*export default function Button({ caption, type = "submit" }) {} default val during obj destructuring*/}
