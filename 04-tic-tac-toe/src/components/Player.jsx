385b2a129c1474ca3855f14f1f2ac947f16829e3
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-26 02:01:50 +0530

    04 - add input field Edit/Save button; input field doesn't save to variable yet

--- 04-tic-tac-toe/src/components/Player.jsx
+++ 04-tic-tac-toe/src/components/Player.jsx
@@ -1,10 +1,26 @@
+import {useState} from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick(name) {
+        console.log('clicked on button', name);
+        // setIsEditing(!isEditing);
+        // setIsEditing(!isEditing); // these are scheduled and with a delay
+        setIsEditing(wasEditing => !wasEditing); // best practice when current depends on prev state instead of !isEditing
+        // setIsEditing(wasEditing => !wasEditing); // nothing happens when both are functions
+    }
+    // let playerName = (<span className="player-name">{name}</span>)
+    // if(isEditing) {
+    //     playerName = (<input type="text" required></input>);
+    // }
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {!isEditing && (<span className="player-name">{name}</span>)}
+              {/*  {playerName}*/}
               <span className="player-symbol">{symbol}</span>
-              <button>Edit</button>
+                {isEditing && (<input type="text" value={name} required></input>)}
+              <button onClick={()=>handleEditClick(name)}>{isEditing ? "Save" : "Edit"}</button>
             </span>
         </li>
     );
