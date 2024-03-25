f5a04760d8faf36b7e7d49a19ea20ec81b3aa12a
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-25 18:34:10 +0530

    03: useState hook to alter content dynamically

--- 03-react-fundamentals/src/App.jsx
+++ 03-react-fundamentals/src/App.jsx
@@ -3,16 +3,23 @@ import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from './components/TabButton'
 
+import {useState} from "react"; // all functions use... are react hooks, called inside component fns/hooks on top-level
+
 // use capital letters for component like App and return render-able HTML
 
 
 function App() {
-    let tabContent = 'Please click a button'
+    const [selectedTopic, setSelectedTopic] = useState('Please click a button');  // returns stateArray of size 2
+    // let tabContent = 'Please click a button'
     function handleSelect(selectedButton) {
         // Components, JSX, props, state
         console.log(selectedButton);
-        tabContent = selectedButton;
+        // tabContent = selectedButton;
+        setSelectedTopic(selectedButton); // setSelectedTopic tells react to update/render it again
+        console.log(selectedTopic); // here updatedState won't be available immediately
     }
+
+    console.log('Execute/Render App.js again');
 
     return (
         <div>
@@ -44,7 +51,8 @@ function App() {
                         <TabButton onClick={()=>handleSelect('props')}>Props</TabButton>
                         <TabButton onClick={()=>handleSelect('state')}>State</TabButton>
                     </menu>
-                    {tabContent}
+                    {/*{tabContent}*/}
+                    {selectedTopic}
                 </section>
             </main>
         </div>
