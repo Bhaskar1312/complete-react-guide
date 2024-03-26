2a160756ce1c4a70630e64bd4570d4880b946a6e
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-26 15:38:49 +0530

    04: disable buttons if player already selected them in past

--- 04-tic-tac-toe/src/components/GameBoard.jsx
+++ 04-tic-tac-toe/src/components/GameBoard.jsx
@@ -19,7 +19,9 @@ export default function GameBoard({onSelectSquare, turns}) {
                 (row, rowIndex)=><li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex)=> <li key={colIndex}>
-                            <button onClick={()=>onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={()=>onSelectSquare(rowIndex, colIndex)}
+                             disabled={playerSymbol!==null}>{playerSymbol}</button>
+                        {/*    playerSymbol === O or X disable button*/}
                         </li> )}
                     </ol>
                 </li> )}
