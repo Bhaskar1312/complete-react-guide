cfd0bcb44fa3a3e97ac231e894ef3f19a557fedf
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-26 19:22:12 +0530

    05 - take inpt, get output to result console

--- 05-investment-calculator/src/App.jsx
+++ 05-investment-calculator/src/App.jsx
@@ -1,15 +1,33 @@
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 import Results from "./components/Results.jsx";
+import {useState} from "react";
 
 function App() {
-  return (
-      <>
-        <Header></Header>
-        <UserInput></UserInput>
-        <Results></Results>
-      </>
-  )
+    const [userInput, setUserInput] =useState({
+        initialInvestment: 1000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        console.log(inputIdentifier, newValue);
+        setUserInput(prevUserInput=>{
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue,
+            };
+        })
+    }
+
+    return (
+        <>
+            <Header></Header>
+            <UserInput userInput={userInput} onChangeFn={handleChange}></UserInput>
+            <Results input={userInput}></Results>
+        </>
+    )
 }
 
 export default App
--- 05-investment-calculator/src/components/Results.jsx
+++ 05-investment-calculator/src/components/Results.jsx
@@ -1,3 +1,7 @@
-export default function Results() {
+import {calculateInvestmentResults} from "../util/investment.js";
+
+export default function Results({input}) {
+    const resultData = calculateInvestmentResults(input);
+    console.log(resultData);
     return (<p>Results</p>);
 }
