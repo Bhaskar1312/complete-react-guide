81246805166f94b8f6eaff3ef15d2b2015c4dbb9
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-27 13:05:33 +0530

    06 - styled components incomplete

--- 06-styling-react-apps-with-css/src/App.jsx
+++ 06-styling-react-apps-with-css/src/App.jsx
@@ -1,5 +1,6 @@
 import AuthInputs from './components/AuthInputs.jsx';
 import Header from './components/Header.jsx';
+import Button from "./components/Button.jsx";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Header />
       <main>
         <AuthInputs />
+          {/*<Button ><button /></Button>*/}
       </main>
     </>
   );
