0fb920579c80200ae60737389216373d189c7fc6
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-27 16:50:31 +0530

    08- bug entered name character by character delete

--- 08-refs and portals/src/components/Player.jsx
+++ 08-refs and portals/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import {useState} from "react";
+
 export default function Player() {
+    const [enteredPlayerName, setEnteredPlayerName] = useState();
+    const [submitted, setSubmitted] = useState(false);
+
+    function handleChange(event) {
+        console.log("event.target.value", event.target.value);
+        setEnteredPlayerName(event.target.value);
+    }
+    function handleSubmit() {
+        setSubmitted(true);
+    }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted?enteredPlayerName:"unknown entity"}</h2>
+
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange}/>
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
