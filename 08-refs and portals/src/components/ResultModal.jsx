83677188b9d38e9eb9e8f44719a33805c50e7648
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-27 20:07:49 +0530

    08- forwarding refs in react

--- 08-refs and portals/src/components/ResultModal.jsx
+++ 08-refs and portals/src/components/ResultModal.jsx
@@ -1,6 +1,8 @@
-export default function ResultModal({result, targetTime}) {
-    return (<dialog className="result-modal" open>
-        {/*built-in dialog buy default is not visible, so open in <dialog className="result-modal" open>*/}
+import {forwardRef} from "react";
+const ResultModal = forwardRef( function ResultModal({result, targetTime}, ref) {
+    return (<dialog ref={ref} className="result-modal" open>
+        {/*built-in dialog buy default is not visible, so open in <dialog className="result-modal" open>
+        but doing it directly won't blur the background and we need to pass ref*/}
         <h2>You {result}</h2>
         <p>The targetTime was <strong>{targetTime}</strong> seconds.</p>
         <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -10,3 +12,5 @@ export default function ResultModal({result, targetTime}) {
         </form>
     </dialog>)
 }
+)
+export default ResultModal;
--- 08-refs and portals/src/components/TimerChallenge.jsx
+++ 08-refs and portals/src/components/TimerChallenge.jsx
@@ -6,10 +6,12 @@ export default function TimerChallenge({title, targetTime}) {
     const [timerStarted, setTimerStarted] = useState(false);
 
     const timer = useRef(); // store state, but wouldn't cause react to re-execute DOM again
+    const dialog = useRef();
     function handleStart() {
         setTimerStarted(true);// this can also be set after setTimeout instead
         timer.current = setTimeout(()=> {
             setTimerExpired(true);
+            dialog.current.showModal();
         }, targetTime*1000)
     }
 
@@ -20,7 +22,7 @@ export default function TimerChallenge({title, targetTime}) {
 
     return (
         <>
-        {timerExpired && <ResultModal targetTime={targetTime} result="lost"/>}
+        <ResultModal ref={dialog} targetTime={targetTime} result="lost"/>
         <section className="challenge">
             <h2>{title}</h2>
             {/*{timerExpired && <p>You lost! </p>}*/}
@@ -30,8 +32,8 @@ export default function TimerChallenge({title, targetTime}) {
             <button onClick={timerStarted? handleStop:handleStart}>
                 {timerStarted? 'Stop' :'Start'} Challenge
             </button>
-            <p className="">
-                Timer is running.../ Timer inactive
+            <p className={timerStarted? 'active': undefined}>
+                { timerStarted ? 'Timer is running...': 'Timer inactive' }
             </p>
         </section>
         </>
