6744a3d527f2fe73748f20116037bb228ff19b02
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-03-31 13:57:22 +0530

    11 - use Callback as Safety against infinite loop

--- 11-placepicker sideEffects useEffect/src/App.jsx
+++ 11-placepicker sideEffects useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -76,7 +76,7 @@ function App() {
 
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -85,7 +85,7 @@ function App() {
 
     const storeIds = JSON.parse(localStorage.getItem('selectedItems')) || [];
     localStorage.setItem('selectedItems', JSON.stringify(storeIds.filter((id)=>id!==selectedPlace.current)))
-  }
+  }, []);
 
   return (
     <>
