0d5de314766aa3c718c9878c52fa58ac83adc442
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-04-07 17:39:53 +0530

    12- extract to new component, duplicate key - two timers bug

---
+++ 12-quiz-app/src/components/Answers.jsx
@@ -0,0 +1,32 @@
+import QUESTIONS from "../questions.js";
+import {useRef} from "react";
+
+export default function Answers({answers, selectedAnswer, answerState, onSelect: handleSelectAnswer}) {
+    const shuffledAnswers = useRef(); // not change the element
+    if(!shuffledAnswers.current) { // only when undefined, shuffle the array
+        shuffledAnswers.current = [...answers];
+        shuffledAnswers.current.sort(() => Math.random()-0.5); // swap, [-1, 1]
+    }
+
+    return (
+        <ul id="answers">
+            {shuffledAnswers.current.map((answer) =>{
+                    let cssClass = '';
+                    const isSelected = selectedAnswer === answer;
+                    if(answerState==='answered' && isSelected) {
+                        cssClass='selected';
+                    }
+                    if((answerState==='correct' || answerState==='wrong') && isSelected) {
+                        cssClass=answerState;
+                    }
+                    return (<li key={answer} className="answer">
+                        <button
+                            className={cssClass}
+                            onClick={()=>handleSelectAnswer(answer)}>
+                            {answer}</button>
+                    </li>)
+                }
+            )}
+        </ul>
+    );
+}
--- 12-quiz-app/src/components/Quiz.jsx
+++ 12-quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,11 @@
-import {useCallback, useRef, useState} from "react";
+import {useCallback, useState} from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
+import Answers from "./Answers.jsx";
 
 export default function Quiz() {
     // const [activeQuestionIndex, setActiveQuestionIndex] =  useState(0);
-    const shuffledAnswers = useRef(); // not change the element
     const [answerState, setAnswerState] = useState('')
     const [userAnswers, setUserAnswers] = useState([]);
 
@@ -39,11 +39,6 @@ export default function Quiz() {
         );
     }
 
-    if(!shuffledAnswers.current) { // only when undefined, shuffle the array
-        shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
-        shuffledAnswers.current.sort(() => Math.random()-0.5); // swap, [-1, 1]
-    }
-
     return (
         <div id="quiz">
         <div id="question">
@@ -51,25 +46,13 @@ export default function Quiz() {
                 key={activeQuestionIndex}
                 timeout={10000} onTimeout={handleSkipAnswer}/>
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {shuffledAnswers.current.map((answer) =>{
-                    let cssClass = '';
-                    const isSelected = userAnswers[userAnswers.length-1] === answer;
-                    if(answerState==='answered' && isSelected) {
-                        cssClass='selected';
-                    }
-                    if((answerState==='correct' || answerState==='wrong') && isSelected) {
-                        cssClass=answerState;
-                    }
-                    return (<li key={answer} className="answer">
-                        <button
-                            className={cssClass}
-                            onClick={()=>handleSelectAnswer(answer)}>
-                            {answer}</button>
-                    </li>)
-                }
-                )}
-            </ul>
+            <Answers
+                key={activeQuestionIndex} // with key, when it changes, it is remounted
+                answers={QUESTIONS[activeQuestionIndex].answers}
+                selectedAnswer={userAnswers[userAnswers.length-1]}
+                answerState={answerState}
+                onSelect={handleSelectAnswer}
+            />
         </div>
 </div>
     );
