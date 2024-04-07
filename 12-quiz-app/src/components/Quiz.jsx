bec9598b270e9c4da43645d2ae90548e1644f1f1
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-04-07 13:26:55 +0530

    12-shuffled answers as options

--- 12-quiz-app/src/components/Quiz.jsx
+++ 12-quiz-app/src/components/Quiz.jsx
@@ -1,10 +1,14 @@
 import {useState} from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
     // const [activeQuestionIndex, setActiveQuestionIndex] =  useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
+
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex===QUESTIONS.length;
+
 
     function handleSelectAnswer(answer) {
         setUserAnswers(prevAnswers => {
@@ -12,17 +16,29 @@ export default function Quiz() {
         });
     }
 
+    if(quizIsComplete) {
+        return (
+          <div id="summary">
+              <img src={quizCompleteImg} alt="Trophy icon" />
+              <h2>Quiz is Completed!</h2>
+          </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random()-0.5); // swap, [-1, 1]
+
     return (
         // <div id="quiz"></div>
         <div id="question">
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map((answer) =>
+                {shuffledAnswers.map((answer) =>
                     (<li key={answer} className="answer">
                         <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
                     </li>)
                 )}
             </ul>
         </div>
-    )
+    );
 }
