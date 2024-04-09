acd8af5a1827a169ccbe3e3542b0498a0a8439c1
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-04-09 14:55:31 +0530

    15-fetching the places

--- 15-connecting-to-database/src/components/AvailablePlaces.jsx
+++ 15-connecting-to-database/src/components/AvailablePlaces.jsx
@@ -1,10 +1,27 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // cant use await as AvailablePlaces is not async,
+  // fetch('http://localhost:3000/places')
+  //     .then((res)=> res.json()) // again promise
+  //     .then((respData)=> respData.places)
+  //     .then((resPlace)=>setAvailablePlaces(resPlace)); // here it updates state, renders component, update state, ... inf loop
+
+
+  useEffect(()=>{
+    // cant use await as AvailablePlaces is not async,
+    fetch('http://localhost:3000/places')
+        .then((res)=> res.json()) // again promise
+        .then((respData)=> setAvailablePlaces(respData.places))
+  }, []); // only once render
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
