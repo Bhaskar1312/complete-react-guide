762419110d1f251daff197cb75d67985294d37ee
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-04-11 13:46:22 +0530

    15-extract to custom hook and use it

---
+++ 16-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+// hooks can be used in custom hooks or react components. useXxx for naming
+import {useEffect, useState} from "react";
+
+export function useFetch(fetchFn, initialValue) {
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initialValue);
+    useEffect(() => {
+        async function fetchData() {
+            setIsFetching(true);
+            try {
+                const data = await fetchFn();
+                setFetchedData(data);
+            } catch (error) {
+                setError({ message: error.message || 'Failed to fetch data.' });
+            }
+
+            setIsFetching(false);
+        }
+
+        fetchData();
+
+    }, [fetchFn]);
+
+    return {
+        isFetching,
+        fetchedData,
+        error
+    }
+}
